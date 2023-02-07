06154baa6abd22cebc95101c8d6059ad819f2673
kengo <mathematics9973@gmail.com>
2023-02-07 23:49:09 +0900

    アソシエーション設定

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
   has_many :favorites, dependent:destroy
+  has_many *favorite_user, through: :favorites, source: :user
 end
