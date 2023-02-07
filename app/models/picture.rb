f68276d78bcac75f559278b2d8da47f8a1f0b8fa
kengo <mathematics9973@gmail.com>
2023-02-08 00:50:52 +0900

    バリデーション設定

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
   mount_uploader :image, ImageUploader
+  belongs_to :user
   has_many :favorites, dependent:destroy
-  has_many *favorite_user, through: :favorites, source: :user
+  has_many :favorite_user, through: :favorites, source: :user
+  validates :content, presence: true,length: { minimum: 1, maximum: 140 }
 end
